0b2a8924f74e7ebc629244861afe5f84a55651f9
KehkashanImran <125457216+KehkashanImran@users.noreply.github.com>
2023-04-12 03:28:36 +0500

    Task 2

---
+++ task1.js
@@ -0,0 +1,92 @@
+//QUESTION 1
+    /*  
+    function create(num1){
+        //let num2=24;
+        return function(num2){
+console.log( num1+num2)
+        }
+
+    }
+    let result1=create(9);
+    let result2=create(5);
+    create(6)
+    create(4)
+    */
+   //ASSIGNMENT 2
+   /* 
+   function searchArray(arr, value) {
+//     if (!arr.length) {
+//         return false
+//     }    
+//     else if (arr[0] === value) {
+//         return true
+//     }
+//     else {
+//         return searchArray(arr.slice(1), value)
+//     }
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let value = 4;
+// console.log(searchArray(arr, value)); 
+// value = 11;
+// console.log(searchArray(arr, value)); 
+
+   
+   */
+
+//QUESTION 3
+/* 
+function addParagraph(contentPara) {
+// const paragraph = document.createElement("p"); 
+// const contentPara = document.createTextNode(contentPara); 
+// paragraph.appendChild(text); 
+// document.body.appendChild(paragraph);
+// }
+
+//QUESTION 4
+
+function addListItem(text) {
+//     const list = document.querySelector('ul');
+//     const listItem = document.createElement('li');
+//     listItem.textContent = text;
+//     list.appendChild(listItem)
+
+
+//QUESTOIN 5
+/* 
+let myPara = document.getElementById("my-para");
+// myPara.innerText = "Hello world";
+// myPara.innerHTML = "<b>Hello PAKISTAN!</b>"
+// console.log(myPara);
+// myPara.innerHTML = "<b>Hello PAKISTAN!</b>";
+// myPara.style.backgroundColor = "aqua";
+// myPara.style.color = "#ff0000";
+
+//QUESTION 6
+/* 
+let prevStudents = localStorage.getItem("students"); // get from LocalStorage
+let students = prevStudents ? JSON.parse(prevStudents) : [];
+function studentRecord() {
+  let std = {
+    name: prompt("Enter Name"),
+    rollNo: +prompt("Enter Roll no"),
+    teacher: prompt("Enter Teachers Name"),
+    className: prompt("Enter Your Class"),
+  };
+  students.push(std);
+  console.log(students);
+  let stringify = JSON.stringify(students); // Convert it to String
+  localStorage.setItem("students", stringify); // Save it to LocalStorage
+}
+
+
+
+
+
+*/
+
+*/
+
+
+
+*/
